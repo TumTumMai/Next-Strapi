3b120085596036c0e606da8590bd7957e6be65bf
TumTumMai <toongzaza@hotmail.com>
2022-01-08 19:30:02 +0700

    Edit_hello_FC

--- fronend/src/components/hello.tsx
+++ fronend/src/components/hello.tsx
@@ -1,13 +1,14 @@
-const Hello = ({
-    text
-}: {
-    text: string
-}): React.ReactElement => {
-    return (
-        <div>
-            {text}
-        </div>
-    )
+import { FC } from "react";
+
+// const Hello = ({ text }: { text: string }): React.ReactElement => {
+//   return <div>{text}</div>;
+// };
+interface IProps {
+  text: string;
 }
 
-export default Hello
+const Hello: FC<IProps> = (props) => {
+  return <div>{props.text}</div>;
+};
+
+export default Hello;
